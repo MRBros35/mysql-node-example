5d9065b83f02956316c1ea80f5ce3ae4c636ee28
Michael Rothkopf <michaelhrothkopf@gmail.com>
2021-03-13 00:22:41 -0500

    Cleaned up and documented code
    I just did some tidying up, fixed some glaring holes in the server, and commented what each line does and, if necessary, why I included it.

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-var mysql = require("mysql");
-var express = require('express');
-var app = express();
+var mysql = require("mysql"); // Require the MySQL connection library
+var express = require('express'); // Require the Express API creation library
+var app = express(); // Create an instance of Express
 
-var con = mysql.createConnection({
+const path = require("path");
+
+var con = mysql.createConnection({ // Configure a connection to the database
     host: "localhost",
-    user: "root",
-    password: "",
+    user: "root", // Don't use root
+    password: "", // Please set a password
     database: "test"
 });
 
-app.use('/api/v1/get_test_data', (req, res) => {
-    con.connect(function(err) {
-        if (err) throw err;
-        con.query("SELECT * FROM test_data;", (err, result) => {
-            //if (err) throw err;
-            res.send(result);
-        });
-    });
+con.connect(function(err) { // Connect the configured connection to the database
+	if (err) console.log(err); // I don't throw errors, a lot are extraneous
 });
 
-app.use('/', (req, res) => {
-	res.sendFile(__dirname + '/static/html/index.html');
+app.get('/api/v1/testData', (req, res) => { // Have Express listen for GET requests on the URL "/api/v1/testData"
+    con.query("SELECT * FROM test_data;", (err, result) => { // Select all columns from all rows from the table test_data and return it to an anonymous callback function
+		if (err) console.log(err); // Again, I don't throw errors due to relevancy
+		res.send(result); // Send the result back to the client
+	});
 });
 
-var server = app.listen(3000, () => {
-    var host = server.address().address;
-    var port = server.address().port;
+app.get('/', (req, res) => { // Have Express listen for GET requests on the URL "/"
+	res.sendFile(path.resolve('static/html/index.html')); // Send the code at the resolved path of the static "index.html" file
+});
 
-    console.log("Listening on http://%s:%s", host, port);
+var server = app.listen(5000, () => { // Have Express listen on port 5000 and store the result in the variable server. When the connection is established, callback this anonymous function.
+	let addr = server.address(); // Get the listening address
+    var host = addr.address; // Get the hostname of the listening address
+    var port = addr.port; // Get he port of the listening address
+
+    console.log("Listening on http://%s:%s", host, port); // Output the listening address to the console
 });
